bd4f9b19566a6c9125586082abe8c08eb75c3166
vigneshwaran <vigneshvgm557@gmail.com>
2023-07-19 23:50:43 +0530

    rectified path problem after deployment

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<CommonAppbar />}>
            <Route index element={< App/>} />
